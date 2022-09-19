eb6204a47228a8b385b744fae2724d0deb036e76
SimonB50 <szymon.dawid@gmail.com>
2022-09-19 21:43:20 +0200

    Export typów

---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+export * from './ActivityData';
+export * from './Exceptions';
+export * from './GameData';
+export * from './ServerData';
+export * from './SocketData';
+export * from './StatusData';
