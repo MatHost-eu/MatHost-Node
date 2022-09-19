c4421d3efbbbf653cb3c4de7b0e59690c6c6488a
SimonB50 <szymon.dawid@gmail.com>
2022-09-19 14:27:08 +0200

    JSDocs i małe poprawki

--- src/classes/PteroSettingsManager.ts
+++ src/classes/PteroSettingsManager.ts
@@ -3,6 +3,15 @@ import { fetch } from 'undici';
 import { PteroServer } from './PteroServer';
 import { APIException } from '../types/Exceptions';
 
+/**
+ * Klasa służąca do zarządzania ustawieniami serwera.
+ * @class PteroSettingsManager
+ * @example
+ * const settings = new PteroSettingsManager(server);
+ * @param {PteroServer} pteroServer - Serwer, którego ustawienia mają być zarządzane
+ * @property {PteroServer} pteroServer - Serwer, którego ustawienia mają być zarządzane
+ * @property {string} #apiKey - Klucz API
+ */
 export class PteroSettingsManager {
   pteroServer: PteroServer;
   #apiKey: string;
@@ -11,16 +20,42 @@ export class PteroSettingsManager {
     this.pteroServer = pteroServer;
   }
 
+  /**
+   * Autoryzuje się kluczem API
+   * @function
+   * @example
+   * const settings = new PteroSettingsManager(server);
+   * settings.authorize('API_KEY');
+   * @param {string} apiKey - Klucz API
+   * @return boolean
+   */
   authorize(apiKey: string): boolean {
     this.#apiKey = apiKey;
     return true;
   }
 
+  /**
+   * Przerywa autoryzację
+   * @function
+   * @example
+   * const settings = new PteroSettingsManager(server);
+   * settings.unAuthorize();
+   * @return boolean
+   */
   unAuthorize(): boolean {
     this.#apiKey = '';
     return true;
   }
 
+  /**
+   * Zmienia nazwę serwera
+   * @function
+   * @example
+   * const settings = new PteroSettingsManager(server);
+   * settings.renameServer('Serwer Minecraft');
+   * @param {string} name - Nowa nazwa serwera
+   * @return {Promise<boolean | APIException>}
+   */
   async renameServer(name: string): Promise<boolean | APIException> {
     const apiResponse = await fetch(`https://ptero.mathost.eu/server/${this.pteroServer.serverId}/settings/rename`, {
       method: 'POST',
@@ -67,6 +102,14 @@ export class PteroSettingsManager {
     }
   }
 
+  /**
+   * Przeprowadza ponowną instalację serwera
+   * @function
+   * @example
+   * const settings = new PteroSettingsManager(server);
+   * settings.reinstallServer();
+   * @return {Promise<boolean | APIException>}
+   */
   async reinstallServer(): Promise<boolean | APIException> {
     const apiResponse = await fetch(`https://ptero.mathost.eu/server/${this.pteroServer.serverId}/settings/reinstall`, {
       method: 'POST',
@@ -111,6 +154,15 @@ export class PteroSettingsManager {
     }
   }
 
+  /**
+   * Zmienia obraz dockera serwera
+   * @function
+   * @example
+   * const settings = new PteroSettingsManager(server);
+   * settings.changeDockerImage('ghcr.io/parkervcp/yolks:nodejs_16');
+   * @param {string} dockerImage - Nowy obraz dockera
+   * @return {Promise<boolean | APIException>}
+   */
   async setDocketImage(dockerImage: string): Promise<boolean | APIException> {
     const apiResponse = await fetch(`https://ptero.mathost.eu/server/${this.pteroServer.serverId}/settings/docker-image`, {
       method: 'POST',
--- src/classes/PteroSocket.ts
+++ src/classes/PteroSocket.ts
@@ -4,6 +4,18 @@ import WebSocket from 'ws';
 import { EventEmitter } from 'node:events'
 import { APIException } from '../types/Exceptions';
 
+/**
+ * Klasa służąca do interakcji z API WebSocketa panelu serwera MatHost.eu
+ * @class PteroSocket
+ * @extends EventEmitter
+ * @example
+ * const socket = new PteroSocket(server);
+ * @param {PteroServer} pteroServer Serwer, z którego pobierane są dane
+ * @property {PteroServer} pteroServer Serwer, z którego pobierane są dane
+ * @property {WebSocket} socket WebSocket
+ * @property {string} socketUrl Adres URL WebSocketa
+ * @property {string} #socketToken Token WebSocketa
+ */
 export class PteroSocket extends EventEmitter {
   pteroServer: PteroServer;
   socket: WebSocket;
@@ -14,8 +26,18 @@ export class PteroSocket extends EventEmitter {
     super();
 
     this.pteroServer = pteroServer;
+    this.socketUrl = '';
+    this.#socketToken = '';
   }
 
+  /**
+   * Łączy się z WebSocketem panelu serwera
+   * @function
+   * @example
+   * const socket = new PteroSocket(server);
+   * socket.connect();
+   * @return {Promise<void | APIException>}
+   */
   async connect(): Promise<void | APIException> {
     const socketData = await this.pteroServer.getSocketData();
 
@@ -48,6 +70,13 @@ export class PteroSocket extends EventEmitter {
     });
   }
 
+  /**
+   * Odczytuje wiadomość z WebSocketa
+   * @function
+   * @param data - Dane otrzymane od WebSocketa
+   * @private
+   * @return {Promise<void | APIException>}
+   */
   async #readMessage(data: any): Promise<void | APIException> {
     const { event, args } = JSON.parse(data.toString());
     switch (event) {
@@ -71,40 +100,104 @@ export class PteroSocket extends EventEmitter {
     }
   }
 
-  send(data: any): void {
-    this.socket.send(JSON.stringify(data));
-  }
-
+  /**
+   * Regeneruje token autorizacyjny WebSocketa
+   * @function
+   * @example
+   * const socket = new PteroSocket(server);
+   * socket.regenToken();
+   * socket.connect();
+   * @return {Promise<SocketData | APIException>}
+   */
   async regenToken(): Promise<void | APIException> {
     const newSocketData = await this.pteroServer.getSocketData();
     if (newSocketData as APIException) return newSocketData as APIException;
 
     this.#socketToken = (newSocketData as SocketData).token;
-
-    this.sendAuth();
   }
 
+  /**
+   * Wysyła wiadomość do WebSocketa
+   * @function
+   * @example
+   * const socket = new PteroSocket(server);
+   * socket.connect();
+   *
+   * socket.send({
+   *   event: 'send command',
+   *   args: 'say Hello World!'
+   * })
+   * @param {Record<string, any>} data - Dane, które mają zostać wysłane
+   * @return {void}
+   */
+  send(data: Record<string, any>): void {
+    this.socket.send(JSON.stringify(data));
+  }
+
+  /**
+   * Wysyła wiadomość z tokenem autoryzacyjnym
+   * @function
+   * @example
+   * const socket = new PteroSocket(server);
+   * socket.connect();
+   *
+   * // Ta metoda jest wywoływana automatycznie, więc używanie jej nie jest konieczne
+   * socket.sendAuth();
+   * @return {void}
+   */
   sendAuth(): void {
-    this.send(JSON.stringify({
+    this.send({
       event: 'auth',
       args: this.#socketToken
-    }))
+    });
   }
 
+  /**
+   * Wysyła komendę do konsoli serwera
+   * @function
+   * @example
+   * const socket = new PteroSocket(server);
+   * socket.connect();
+   *
+   * socket.sendCommand('say Hello World!');
+   * @param {string} command - Komenda, która ma zostać uruchomiona na serwerze
+   * @return {void}
+   */
   sendCommand(command: string): void {
-    this.send(JSON.stringify({
+    this.send({
       event: 'send command',
       args: command
-    }));
+    });
   }
 
+  /**
+   * Wykonuje akcje na serwerze
+   * @function
+   * @example
+   * const socket = new PteroSocket(server);
+   * socket.connect();
+   *
+   * socket.sendPowerAction('start');
+   * @param {"start" | "stop" | "restart" | "kill"} action - Akcja, która ma zostać wykonana na serwerze
+   * @return {void}
+   */
   sendPowerAction(action: "start" | "stop" | "restart" | "kill"): void {
-    this.send(JSON.stringify({
+    this.send({
       event: 'set state',
       args: action
-    }));
+    });
   }
 
+  /**
+   * Zamyka połączenie z WebSocketem
+   * @function
+   * @example
+   * const socket = new PteroSocket(server);
+   * socket.connect();
+   *
+   * socket.close();
+   * @return {void}
+   */
   close(): void {
     this.socket.close();
     this.socket = undefined;
