125aa8c5b36b27d9010279b953c0af43deec59a9
SimonB50 <szymon.dawid@gmail.com>
2022-09-20 07:54:51 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+// Export classes
 export * from './classes/PteroServer';
 export * from './classes/PteroSocket';
 export * from './classes/PteroSettingsManager';
+
+// Export types
+export * from './types';
